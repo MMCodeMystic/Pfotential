6f4703f4e1044e3fe397cd9bc1e30e68c35c32ad
Markus Meier <markus.meier@gfn.education>
2026-09-17 16:02:31 +0200

    Particles, Spotlight implementiert

---
+++ main.js
@@ -0,0 +1,4 @@
+document.addEventListener('pointermove', e => {
+    document.documentElement.style.setProperty('--mx', e.clientX + 'px');
+    document.documentElement.style.setProperty('--my', e.clientY + 'px');
+});
